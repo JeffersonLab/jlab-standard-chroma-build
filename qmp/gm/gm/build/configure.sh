8be88a303736b09e04fed4891adf03382d0eac28
bjoo <bjoo>
2006-04-04 21:23:07 +0000

    Cleans before configuring

--- qmp/gm/gm/build/configure.sh
+++ qmp/gm/gm/build/configure.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-../../../qmp/configure --prefix=/usr/local/qmp/mpich-gm-1.2.5 --with-qmp-comms-type=MPI \
+. ../../../../functions.sh
+PREFIX=`make_prefix ../../../PREFIX ../../../VERSION mpich-gm-1.2.5`
+clean_dir ${PREFIX}
+
+../../../qmp/configure --prefix=${PREFIX} --with-qmp-comms-type=MPI \
     --with-qmp-comms-cflags="-I/usr/local/mpich-gm-1.2.5/include" \
     --with-qmp-comms-ldflags="-L/usr/local/mpich-gm-1.2.5/lib -L/usr/local/gm-1.6.4/lib" \
     --with-qmp-comms-libs="-lmpich -lgm -lpthread" CFLAGS="-O2"
