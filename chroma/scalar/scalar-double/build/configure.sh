7049016e5370c85e2d7911765af6a78d9c3c609f
bjoo <bjoo>
2006-04-06 19:08:31 +0000

    All of these should now clean at configure and use the VERSION and PREFIX files

--- chroma/scalar/scalar-double/build/configure.sh
+++ chroma/scalar/scalar-double/build/configure.sh
@@ -1,4 +1,14 @@
-#!/bin/tcsh
+#!/bin/bash
 
-../../../chroma/configure --prefix=/usr/local/chroma/scalar-double --with-qdp=/usr/local/qdp++/scalar-double CFLAGS="-O2 -msse -msse2 -march=pentium4" CXXFLAGS="" LIBS="-lgmp" --enable-sse-wilson-dslash  --with-gmp=/usr --enable-opt-cfz-linop
+ARCH=scalar-double
+CHROMADIR=../../..
+ROOTDIR=../../../..
+QDPDIR=${ROOTDIR}/qdp++
 
+. ${ROOTDIR}/functions.sh
+PREFIX=`make_prefix ${CHROMADIR}/PREFIX ${CHROMADIR}/VERSION ${ARCH}`
+clean_dir ${PREFIX}
+
+QDP_PREFIX=`make_prefix ${QDPDIR}/PREFIX ${QDPDIR}/VERSION ${ARCH}`
+
+${CHROMADIR}/chroma/configure --prefix=${PREFIX} --with-qdp=${QDP_PREFIX} CFLAGS="-O2 -msse -msse2 -march=pentium4" CXXFLAGS="" LIBS="-lgmp" --enable-sse-wilson-dslash  --with-gmp=/usr 
