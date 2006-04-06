3c8646b5eef984a2150a596c66932137f24dec45
bjoo <bjoo>
2006-04-06 18:51:38 +0000

    Cleans and uses prefix

--- qdp++/scalar/scalar-double/build/configure.sh
+++ qdp++/scalar/scalar-double/build/configure.sh
@@ -7,4 +7,4 @@ ROOTDIR=../../../..
 PREFIX=`make_prefix ${QDPDIR}/PREFIX ${QDPDIR}/VERSION scalar-double`
 clean_dir ${PREFIX}
 
-${QDPDIR}/qdp++/configure --prefix=${PREFX} --enable-parallel-arch=scalar --enable-sse2 CXXFLAGS="-O2 -finline-limit=50000 -msse -march=pentium4" CFLAGS="-O2" --enable-precision=double
+${QDPDIR}/qdp++/configure --prefix=${PREFIX} --enable-parallel-arch=scalar --enable-sse2 CXXFLAGS="-O2 -finline-limit=50000 -msse -march=pentium4" CFLAGS="-O2" --enable-precision=double
