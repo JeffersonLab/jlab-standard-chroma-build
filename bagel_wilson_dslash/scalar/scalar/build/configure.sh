1c4ee16a71329526f91f8cb2b5877e173cf4a94e
bjoo <bjoo>
2006-05-17 21:33:06 +0000

    BAGELDslash

---
+++ bagel_wilson_dslash/scalar/scalar/build/configure.sh
@@ -0,0 +1,13 @@
+
+. ../../../../functions.sh
+PREFIX=`make_prefix ../../../PREFIX ../../../VERSION scalar`
+clean_dir ${PREFIX}
+
+BAGEL=`make_prefix ../../../../bagel/PREFIX ../../../../bagel/VERSION bagel`
+ ../../../bagel_wilson_dslash/configure --prefix=${PREFIX} \
+    --enable-target-cpu=noarch \
+    --enable-comms=none \
+    --enable-allocator=malloc \
+    --enable-precision=single \
+    --with-bagel=${BAGEL}
+    
