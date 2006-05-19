43ba888064ed68eaaf99cb9b6d94b5610333b8c7
edwards <edwards>
2006-05-19 02:16:19 +0000

    Build script for qdp++

---
+++ build_qdp.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -x
+
+./build.sh    qdp++/gigE/parscalar-gigE \
+  	      qdp++/gigE/parscalar-gigE-double \
+	      qdp++/ibg2-mpi/parscalar-ibg2-mpi \
+	      qdp++/ibg2-mpi/parscalar-ibg2-mpi-double \
+	      qdp++/ib-mpi/parscalar-ib-mpi \
+	      qdp++/ib-mpi/parscalar-ib-mpi-double \
+	      qdp++/single/parscalar-single \
+	      qdp++/single/parscalar-single-double \
+	      qdp++/scalar/scalar \
+	      qdp++/scalar/scalar-double
+
