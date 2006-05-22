8b60038500a0e8ee0be9eb3bf2e0dd1ea1a99713
edwards <edwards>
2006-05-22 20:41:40 +0000

    Fixed to use intel.

--- qmp/single/single-intel/build/configure.sh
+++ qmp/single/single-intel/build/configure.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 . ../../../../functions.sh
-PREFIX=`make_prefix ../../../PREFIX ../../../VERSION single-gcc4`
+PREFIX=`make_prefix ../../../PREFIX ../../../VERSION single-intel`
 clean_dir ${PREFIX}
 
 ../../../qmp/configure --prefix=${PREFIX} --with-qmp-comms-type=SINGLE CC=/opt/intel/cc/9.0/bin/icc LIBS="-static"
