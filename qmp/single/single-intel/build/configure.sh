ec374d2e90d2102425a076eec2b7da450a272f69
edwards <edwards>
2006-05-22 16:08:08 +0000

    Fixed

--- qmp/single/single-intel/build/configure.sh
+++ qmp/single/single-intel/build/configure.sh
@@ -4,4 +4,4 @@
 PREFIX=`make_prefix ../../../PREFIX ../../../VERSION single-gcc4`
 clean_dir ${PREFIX}
 
-../../../qmp/configure --prefix=${PREFIX} --with-qmp-comms-type=SINGLE CC=/opt/intel/cc/9.0/bin/icc
+../../../qmp/configure --prefix=${PREFIX} --with-qmp-comms-type=SINGLE CC=/opt/intel/cc/9.0/bin/icc LIBS="-static"
