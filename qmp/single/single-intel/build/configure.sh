097400d5690f542850beb049fdcbb798ad62786f
edwards <edwards>
2006-05-22 15:42:13 +0000

    New Intel scripts

---
+++ qmp/single/single-intel/build/configure.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+. ../../../../functions.sh
+PREFIX=`make_prefix ../../../PREFIX ../../../VERSION single-gcc4`
+clean_dir ${PREFIX}
+
+../../../qmp/configure --prefix=${PREFIX} --with-qmp-comms-type=SINGLE CC=/opt/intel/cc/9.0/bin/icc
