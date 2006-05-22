053f549d5f60f8b807a95fcb34ab11095f27182a
edwards <edwards>
2006-05-22 16:12:38 +0000

    Fix/

--- chroma/single/parscalar-single-intel/build/configure.sh
+++ chroma/single/parscalar-single-intel/build/configure.sh
@@ -11,4 +11,4 @@ clean_dir ${PREFIX}
 
 QDP_PREFIX=`make_prefix ${QDPDIR}/PREFIX ${QDPDIR}/VERSION ${ARCH}`
 
-${CHROMADIR}/chroma/configure --prefix=${PREFIX} --with-qdp=${QDP_PREFIX} CC=/opt/intel/cc/9.0/bin/icc CFLAGS="-O2 -fast -msse -msse2 -march=pentium4" CXX=/opt/intel/cc/9.0/bin/icc CXXFLAGS="" LIBS="-lgmp" --with-gmp=/usr 
+${CHROMADIR}/chroma/configure --prefix=${PREFIX} --with-qdp=${QDP_PREFIX} CC=/opt/intel/cc/9.0/bin/icc CFLAGS="-O2 -fast -msse -msse2 -march=pentium4" CXX=/opt/intel/cc/9.0/bin/icc CXXFLAGS="" LIBS="-lgmp" --with-gmp=/usr  LIBS="-static"
