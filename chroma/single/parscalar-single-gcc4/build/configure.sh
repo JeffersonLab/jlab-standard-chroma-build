7ab8609798c21d1b9437ca5f9d9648326317c021
edwards <edwards>
2006-05-22 14:41:46 +0000

    Added gcc4 versions of qmp, qdp++, and chroma

---
+++ chroma/single/parscalar-single-gcc4/build/configure.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+ARCH=parscalar-single-gcc4
+CHROMADIR=../../..
+ROOTDIR=../../../..
+QDPDIR=${ROOTDIR}/qdp++
+
+. ${ROOTDIR}/functions.sh
+PREFIX=`make_prefix ${CHROMADIR}/PREFIX ${CHROMADIR}/VERSION ${ARCH}`
+clean_dir ${PREFIX}
+
+QDP_PREFIX=`make_prefix ${QDPDIR}/PREFIX ${QDPDIR}/VERSION ${ARCH}`
+
+${CHROMADIR}/chroma/configure --prefix=${PREFIX} --with-qdp=${QDP_PREFIX} CFLAGS="-O2 -msse -msse2 -march=pentium4" CXXFLAGS="" LIBS="-lgmp" --enable-sse-wilson-dslash  --enable-sse-dwf-cg --with-gmp=/usr 
---
+++ qdp++/single/parscalar-single-gcc4/build/configure.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+ARCH=single
+QDPDIR=../../..
+ROOTDIR=../../../..
+QMPDIR=${ROOTDIR}/qmp
+
+. ${ROOTDIR}/functions.sh
+PREFIX=`make_prefix ${QDPDIR}/PREFIX ${QDPDIR}/VERSION parscalar-${ARCH}-gcc4`
+clean_dir ${PREFIX}
+
+QMP_PREFIX=`make_prefix ${QMPDIR}/PREFIX ${QMPDIR}/VERSION ${ARCH}-gcc4`
+
+${QDPDIR}/qdp++/configure --prefix=${PREFIX} --with-qmp=${QMP_PREFIX} --enable-parallel-arch=parscalar --enable-sse2 CXXFLAGS="-O2 -finline-limit=50000 -msse -march=pentium4" CFLAGS="-O2"
