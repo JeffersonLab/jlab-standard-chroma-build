beb9f1bd63822ea6e545bc2df9d93fa47c45aca3
edwards <edwards>
2006-05-22 16:10:53 +0000

    Fixes.

--- qdp++/single/parscalar-single-intel/build/configure.sh
+++ qdp++/single/parscalar-single-intel/build/configure.sh
@@ -11,4 +11,4 @@ clean_dir ${PREFIX}
 
 QMP_PREFIX=`make_prefix ${QMPDIR}/PREFIX ${QMPDIR}/VERSION ${ARCH}-intel`
 
-${QDPDIR}/qdp++/configure --prefix=${PREFIX} --with-qmp=${QMP_PREFIX} --enable-parallel-arch=parscalar --enable-sse2 CXX=/opt/intel/cc/9.0/bin/icc CXXFLAGS="-O2 -fast -finline-functions -fno-fnalias -fargument-noalias-global -ftemplate-depth-1000 -msse -msse2 -march=pentium4" CC=/opt/intel/cc/9.0/bin/icc CFLAGS="-O2"
+${QDPDIR}/qdp++/configure --prefix=${PREFIX} --with-qmp=${QMP_PREFIX} --enable-parallel-arch=parscalar --enable-sse2 CXX=/opt/intel/cc/9.0/bin/icc CXXFLAGS="-O2 -fast -finline-functions -fno-fnalias -fargument-noalias-global -ftemplate-depth-1000 -msse -msse2 -march=pentium4" CC=/opt/intel/cc/9.0/bin/icc CFLAGS="-O2" LIBS="-static"
