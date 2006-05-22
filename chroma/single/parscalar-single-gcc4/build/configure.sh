436dcd09ab162807ab5ce5211839e51814dc1dd5
edwards <edwards>
2006-05-22 15:13:27 +0000

    Point to gcc4

--- chroma/single/parscalar-single-gcc4/build/configure.sh
+++ chroma/single/parscalar-single-gcc4/build/configure.sh
@@ -11,4 +11,4 @@ clean_dir ${PREFIX}
 
 QDP_PREFIX=`make_prefix ${QDPDIR}/PREFIX ${QDPDIR}/VERSION ${ARCH}`
 
-${CHROMADIR}/chroma/configure --prefix=${PREFIX} --with-qdp=${QDP_PREFIX} CFLAGS="-O2 -msse -msse2 -march=pentium4" CXXFLAGS="" LIBS="-lgmp" --enable-sse-wilson-dslash  --enable-sse-dwf-cg --with-gmp=/usr 
+${CHROMADIR}/chroma/configure --prefix=${PREFIX} --with-qdp=${QDP_PREFIX} CC=/usr/local/gcc-4.0.2/bin/gcc CFLAGS="-O2 -msse -msse2 -march=pentium4" CXX=/usr/local/gcc-4.0.2/bin/g++ CXXFLAGS="" LIBS="-lgmp" --enable-sse-wilson-dslash  --enable-sse-dwf-cg --with-gmp=/usr 
