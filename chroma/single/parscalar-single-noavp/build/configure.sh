b5ac4fa981e3114d6ce1fecc4edfb624c5ac2c33
edwards <edwards>
2006-05-26 20:02:16 +0000

    A noavp version.

---
+++ chroma/single/parscalar-single-noavp/build/configure.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+ARCH=parscalar-single
+CHROMADIR=../../..
+ROOTDIR=../../../..
+QDPDIR=${ROOTDIR}/qdp++
+
+. ${ROOTDIR}/functions.sh
+PREFIX=`make_prefix ${CHROMADIR}/PREFIX ${CHROMADIR}/VERSION ${ARCH}-noavp`
+clean_dir ${PREFIX}
+
+QDP_PREFIX=`make_prefix ${QDPDIR}/PREFIX ${QDPDIR}/VERSION ${ARCH}`
+
+${CHROMADIR}/chroma/configure --prefix=${PREFIX} --with-qdp=${QDP_PREFIX} CFLAGS="-O2 -msse -msse2 -march=pentium4" CXXFLAGS="" LIBS="-lgmp" --enable-sse-wilson-dslash  --with-gmp=/usr 
