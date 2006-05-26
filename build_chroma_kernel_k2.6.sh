7d96d66bfb530c4c9b7ce75b044f51d98a7edea4
edwards <edwards>
2006-05-26 20:13:45 +0000

    Combined into one build.sh invocation.

--- build_chroma_kernel_k2.6.sh
+++ build_chroma_kernel_k2.6.sh
@@ -18,16 +18,13 @@ date
 
 ./build.sh  chroma/gigE/parscalar-gigE \
             chroma/gigE/parscalar-gigE-double \
-            chroma/gigE/parscalar-gigE-noavp
-
-./build.sh  chroma/ibg2-mpi/parscalar-ibg2-mpi \
+            chroma/gigE/parscalar-gigE-noavp \
+            chroma/ibg2-mpi/parscalar-ibg2-mpi \
             chroma/ibg2-mpi/parscalar-ibg2-mpi-double \
-            chroma/ibg2-mpi/parscalar-ibg2-mpi-noavp
-
-./build.sh  chroma/single/parscalar-single \
-            chroma/single/parscalar-single-double
-
-./build.sh  chroma/scalar/scalar \
+            chroma/ibg2-mpi/parscalar-ibg2-mpi-noavp \
+            chroma/single/parscalar-single \
+            chroma/single/parscalar-single-double \
+            chroma/scalar/scalar \
             chroma/scalar/scalar-double
 
 echo ""
