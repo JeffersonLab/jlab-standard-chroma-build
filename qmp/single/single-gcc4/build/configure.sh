4a32d3501c945fdeb921b7b247a322cc213bc82e
edwards <edwards>
2006-05-31 14:39:32 +0000

    Changed gcc-4 compiler flag to point to  /usr/local/gcc-4.0.2/bin/gcc

--- qmp/single/single-gcc4/build/configure.sh
+++ qmp/single/single-gcc4/build/configure.sh
@@ -4,4 +4,4 @@
 PREFIX=`make_prefix ../../../PREFIX ../../../VERSION single-gcc4`
 clean_dir ${PREFIX}
 
-../../../qmp/configure --prefix=${PREFIX} --with-qmp-comms-type=SINGLE CC=/usr/bin/gcc4
+../../../qmp/configure --prefix=${PREFIX} --with-qmp-comms-type=SINGLE CC=/usr/local/gcc-4.0.2/bin/gcc
