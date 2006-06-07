5ac854ea38038ae2f73e819713f181ca82589195
bjoo <bjoo>
2006-06-07 14:49:13 +0000

    Initial BGL build

---
+++ qdp++/bgl/parscalar-bgl-double/build/configure.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+ARCH=bgl
+QDPDIR=../../..
+ROOTDIR=../../../..
+. ${ROOTDIR}/functions.sh
+
+PREFIX=`make_prefix ${QDPDIR}/PREFIX ${QDPDIR}/VERSION parscalar-${ARCH}-double-`
+clean_dir ${PREFIX}
+
+QMP_PREFIX=`make_prefix ${ROOTDIR}/qmp/PREFIX ${ROOTDIR}/qmp/VERSION ${ARCH}`
+BAGEL_QDP_PREFIX=`make_prefix ${ROOTDIR}/bagel_qdp/PREFIX ${ROOTDIR}/bagel_qdp/VERSION bgl-double`
+
+
+export PATH=/bgl/BlueLight/ppcfloor/blrts-gnu/bin:$PATH
+
+${QDPDIR}/qdp++/configure --prefix=${PREFIX} \
+			  --host=powerpc-bgl-blrts-gnu \
+			  --build=none \
+			  --with-libxml2=/scratch/bjoo/install/libxml2 \
+			  --with-qmp=${QMP_PREFIX} \
+			  --with-bagel-qdp=${BAGEL_QDP_PREFIX} \
+			  --enable-parallel-arch=parscalar \
+			  --enable-precision=double \
+			  --disable-qmp-route \
+			  --enable-slow-route \
+			   CXXFLAGS="-O2 -finline-limit=50000" \
+			   CFLAGS="-O2"
