7fb6a0a1db78c73a8d69320f67977bf8635122c8
bjoo <bjoo>
2006-06-07 16:15:20 +0000

    Now picks up our own libxml2

--- qdp++/bgl/parscalar-bgl-double/build/configure.sh
+++ qdp++/bgl/parscalar-bgl-double/build/configure.sh
@@ -10,14 +10,15 @@ clean_dir ${PREFIX}
 
 QMP_PREFIX=`make_prefix ${ROOTDIR}/qmp/PREFIX ${ROOTDIR}/qmp/VERSION ${ARCH}`
 BAGEL_QDP_PREFIX=`make_prefix ${ROOTDIR}/bagel_qdp/PREFIX ${ROOTDIR}/bagel_qdp/VERSION bgl-double`
+LIBXML2_PREFIX=`make_prefix ${ROOTDIR}/libxml2/PREFIX ${ROOTDIR}/libxml2/VERSION libxml2`
 
-
+echo $LIBXML2_PREFIX
 export PATH=/bgl/BlueLight/ppcfloor/blrts-gnu/bin:$PATH
 
 ${QDPDIR}/qdp++/configure --prefix=${PREFIX} \
 			  --host=powerpc-bgl-blrts-gnu \
 			  --build=none \
-			  --with-libxml2=/scratch/bjoo/install/libxml2 \
+			  --with-libxml2=${LIBXML2_PREFIX} \
 			  --with-qmp=${QMP_PREFIX} \
 			  --with-bagel-qdp=${BAGEL_QDP_PREFIX} \
 			  --enable-parallel-arch=parscalar \
