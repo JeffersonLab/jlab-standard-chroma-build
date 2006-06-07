dc84e2113d038faadd9932bef1d78172bf48f1ba
bjoo <bjoo>
2006-06-07 16:19:40 +0000

    Added libxml2 into the bgl_build.sh

--- bgl_build.sh
+++ bgl_build.sh
@@ -2,8 +2,10 @@
 
 ./configure --enable-install-root=/scratch/bjoo/install
 
-./build.sh bagel/native/bagel \
+./build.sh libxml2/bgl/bgl \
+           bagel/native/bagel \
 	   bagel_qdp/bgl/bgl-double \
 	   qmp/bgl/bgl \
-	   bagel_wilson_dslash/bgl/parscalar-bgl-double
-
+	   bagel_wilson_dslash/bgl/parscalar-bgl-double \
+	   qdp++/bgl/parscalar-bgl-double \
+	   chroma/bgl/parscalar-bgl-double
