01cd58d0fabd62692d7c7e8a81d6f7f89a6eef64
bjoo <bjoo>
2006-06-07 13:23:44 +0000

    Initial BGL config file

---
+++ qmp/bgl/bgl/build/configure.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+. ../../../../functions.sh
+PREFIX=`make_prefix ../../../PREFIX ../../../VERSION bgl`
+clean_dir ${PREFIX}
+
+export PATH=/bgl/BlueLight/ppcfloor/blrts-gnu/bin:$PATH
+
+../../../qmp/configure --host=powerpc-bgl-blrts-gnu --build=none \
+    --with-qmp-comms-type=MPI \
+    --with-qmp-comms-cflags="-I/bgl/BlueLight/ppcfloor/bglsys/include" \
+    --with-qmp-comms-ldflags="-L/bgl/BlueLight/ppcfloor/bglsys/lib" \
+    --with-qmp-comms-libs="-lmpich.rts -lmsglayer.rts -ldevices.rts -lrts.rts -ldevices.rts -lrts.rts" \
+    --prefix=${PREFIX} \
+    CFLAGS="-O2" 
