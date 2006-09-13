1cb8dcba1fbf926a307477b49547e0c4ca6cbeff
bjoo <bjoo>
2006-09-13 23:14:19 +0000

    Added new cg-dwf

--- chroma/single/parscalar-single-gcc4/build/configure.sh
+++ chroma/single/parscalar-single-gcc4/build/configure.sh
@@ -4,14 +4,15 @@ ARCH=parscalar-single-gcc4
 CHROMADIR=../../..
 ROOTDIR=../../../..
 QDPDIR=${ROOTDIR}/qdp++
+QMPDIR=${ROOTDIR}/qmp
 
 . ${ROOTDIR}/functions.sh
 PREFIX=`make_prefix ${CHROMADIR}/PREFIX ${CHROMADIR}/VERSION ${ARCH}`
 clean_dir ${PREFIX}
 
 QDP_PREFIX=`make_prefix ${QDPDIR}/PREFIX ${QDPDIR}/VERSION ${ARCH}`
-
+QMP_PREFIX=`make_prefix ${QMPDIR}/PREFIX ${QMPDIR}/VERSION single`
 CC=/dist/gcc-4.1.1/bin/gcc
 CXX=/dist/gcc-4.1.1/bin/g++
 
-${CHROMADIR}/chroma/configure --prefix=${PREFIX} --with-qdp=${QDP_PREFIX} CC=${CC} CXX=${CXX} CFLAGS="-O2 -msse -msse2 -march=pentium4" CXXFLAGS="" LIBS="-lgmp" --enable-sse-wilson-dslash  --enable-sse-dwf-cg --with-gmp=/usr
+${CHROMADIR}/chroma/configure --prefix=${PREFIX} --with-qdp=${QDP_PREFIX} CC=${CC} CXX=${CXX} CFLAGS="-O2 -msse -msse2 -march=pentium4" CXXFLAGS="" LIBS="-lgmp" --enable-sse-wilson-dslash  --enable-cg-dwf --with-gmp=/usr --enable-cg-dwf-cpu-arch=sse --with-qmp=${QMP_PREFIX}
--- chroma/single/parscalar-single/build/configure.sh
+++ chroma/single/parscalar-single/build/configure.sh
@@ -4,11 +4,13 @@ ARCH=parscalar-single
 CHROMADIR=../../..
 ROOTDIR=../../../..
 QDPDIR=${ROOTDIR}/qdp++
+QMPDIR=$(ROOTDIR)/qmp
 
 . ${ROOTDIR}/functions.sh
 PREFIX=`make_prefix ${CHROMADIR}/PREFIX ${CHROMADIR}/VERSION ${ARCH}`
 clean_dir ${PREFIX}
 
 QDP_PREFIX=`make_prefix ${QDPDIR}/PREFIX ${QDPDIR}/VERSION ${ARCH}`
+QMP_PREFIX=`make_prefix ${QMPDIR}/PREFIX ${QMPDIR}/VERSION single`
 
-${CHROMADIR}/chroma/configure --prefix=${PREFIX} --with-qdp=${QDP_PREFIX} CFLAGS="-O2 -msse -msse2 -march=pentium4" CXXFLAGS="" LIBS="-lgmp" --enable-sse-wilson-dslash  --enable-sse-dwf-cg --with-gmp=/usr 
+${CHROMADIR}/chroma/configure --prefix=${PREFIX} --with-qdp=${QDP_PREFIX} CFLAGS="-O2 -msse -msse2 -march=pentium4" CXXFLAGS="" LIBS="-lgmp" --enable-sse-wilson-dslash  --enable-cg-dwf --enable-cg-dwf-cpu-arch=sse --with-gmp=/usr --with-qmp=${QMP_PREFIX}
