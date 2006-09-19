ae0b92811ddc5e4fcc1074c47486e434283837a4
bjoo <bjoo>
2006-09-19 13:59:45 +0000

    Changed configure.sh

--- chroma/single/parscalar-single-double/build/configure.sh
+++ chroma/single/parscalar-single-double/build/configure.sh
@@ -13,4 +13,4 @@ clean_dir ${PREFIX}
 QDP_PREFIX=`make_prefix ${QDPDIR}/PREFIX ${QDPDIR}/VERSION ${ARCH}`
 QMP_PREFIX=`make_prefix ${QMPDIR}/PREFIX ${QMPDIR}/VERSION single`
 
-${CHROMADIR}/chroma/configure --prefix=${PREFIX} --with-qdp=${QDP_PREFIX} CFLAGS="-O2 -msse -msse2 -march=pentium4" CXXFLAGS="" LIBS="-lgmp" --enable-sse-wilson-dslash --with-gmp=/usr  --enable-cg-dwf --enable-cg-dwf-cpu-arch=sse --with-qmp=${QMP_PREFIX}
+${CHROMADIR}/chroma/configure --prefix=${PREFIX} --with-qdp=${QDP_PREFIX} CFLAGS="-O2 -msse -msse2 -march=pentium4" CXXFLAGS="" LIBS="-lgmp" --enable-sse-wilson-dslash --with-gmp=/usr  --enable-cg-dwf=sse --with-qmp=${QMP_PREFIX}
--- chroma/single/parscalar-single-gcc4/build/configure.sh
+++ chroma/single/parscalar-single-gcc4/build/configure.sh
@@ -15,4 +15,4 @@ QMP_PREFIX=`make_prefix ${QMPDIR}/PREFIX ${QMPDIR}/VERSION single`
 CC=/dist/gcc-4.1.1/bin/gcc
 CXX=/dist/gcc-4.1.1/bin/g++
 
-${CHROMADIR}/chroma/configure --prefix=${PREFIX} --with-qdp=${QDP_PREFIX} CC=${CC} CXX=${CXX} CFLAGS="-O2 -msse -msse2 -march=pentium4" CXXFLAGS="" LIBS="-lgmp" --enable-sse-wilson-dslash  --enable-cg-dwf --with-gmp=/usr --enable-cg-dwf-cpu-arch=sse --with-qmp=${QMP_PREFIX}
+${CHROMADIR}/chroma/configure --prefix=${PREFIX} --with-qdp=${QDP_PREFIX} CC=${CC} CXX=${CXX} CFLAGS="-O2 -msse -msse2 -march=pentium4" CXXFLAGS="" LIBS="-lgmp" --enable-sse-wilson-dslash  --enable-cg-dwf=sse --with-qmp=${QMP_PREFIX}
