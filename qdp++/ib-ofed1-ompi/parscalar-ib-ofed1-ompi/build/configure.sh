d82bda60b35e0ab5db6975b5674e269de6008654
bjoo <bjoo>
2006-10-23 20:29:58 +0000

    QDP++ build over open MPI single prec

---
+++ qdp++/ib-ofed1-ompi/parscalar-ib-ofed1-ompi/build/configure.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+ARCH=ib-ofed1-ompi
+QDPDIR=../../..
+ROOTDIR=../../../..
+. ${ROOTDIR}/functions.sh
+
+PREFIX=`make_prefix ${QDPDIR}/PREFIX ${QDPDIR}/VERSION parscalar-${ARCH}`
+clean_dir ${PREFIX}
+
+QMP_PREFIX=`make_prefix ${ROOTDIR}/qmp/PREFIX ${ROOTDIR}/qmp/VERSION ${ARCH}`
+
+${QDPDIR}/qdp++/configure --prefix=${PREFIX} --with-qmp=${QMP_PREFIX} --enable-parallel-arch=parscalar --enable-sse2 CXXFLAGS="-O2 -finline-limit=50000 -msse -march=pentium4" CFLAGS="-O2"  \
+--disable-qmp-route --enable-slow-route
