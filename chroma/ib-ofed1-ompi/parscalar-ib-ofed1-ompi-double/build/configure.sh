78a3a6062c185ce1985a41c391c37b18d8f4959b
bjoo <bjoo>
2006-10-24 13:27:56 +0000

    FIxed typo in config file

--- chroma/ib-ofed1-ompi/parscalar-ib-ofed1-ompi-double/build/configure.sh
+++ chroma/ib-ofed1-ompi/parscalar-ib-ofed1-ompi-double/build/configure.sh
@@ -10,6 +10,6 @@ PREFIX=`make_prefix ${CHROMADIR}/PREFIX ${CHROMADIR}/VERSION ${ARCH}`
 clean_dir ${PREFIX}
 
 QDP_PREFIX=`make_prefix ${QDPDIR}/PREFIX ${QDPDIR}/VERSION ${ARCH}`
-QMP_PREFIX=`make_prefix ${QMPDIR}/PREFIX ${QMPDIR}/VERSION ib-ofed1-mpi`
+QMP_PREFIX=`make_prefix ${QMPDIR}/PREFIX ${QMPDIR}/VERSION ib-ofed1-ompi`
 
 ${CHROMADIR}/chroma/configure --prefix=${PREFIX} --with-qdp=${QDP_PREFIX} CFLAGS="-O2 -msse -msse2 -march=pentium4" CXXFLAGS="" LIBS="-lgmp" --enable-sse-wilson-dslash  --with-gmp=/usr --enable-testcase-runner=mpirun_rsh  --enable-cg-dwf=sse --with-qmp=${QMP_PREFIX}
