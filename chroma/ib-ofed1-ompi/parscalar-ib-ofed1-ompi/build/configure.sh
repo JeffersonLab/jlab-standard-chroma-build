e71c5a784167ba0a8f9ef34be667e8698ad5d42e
bjoo <bjoo>
2006-10-24 13:22:52 +0000

    Added OpenMPI chroma configure files

---
+++ chroma/ib-ofed1-ompi/parscalar-ib-ofed1-ompi/build/configure.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+ARCH=parscalar-ib-ofed1-ompi
+CHROMADIR=../../..
+ROOTDIR=../../../..
+QDPDIR=${ROOTDIR}/qdp++
+QMPDIR=${ROOTDIR}/qmp
+. ${ROOTDIR}/functions.sh
+PREFIX=`make_prefix ${CHROMADIR}/PREFIX ${CHROMADIR}/VERSION ${ARCH}`
+clean_dir ${PREFIX}
+
+QDP_PREFIX=`make_prefix ${QDPDIR}/PREFIX ${QDPDIR}/VERSION ${ARCH}`
+QMP_PREFIX=`make_prefix ${QMPDIR}/PREFIX ${QMPDIR}/VERSION ib-ofed1-mpi`
+
+${CHROMADIR}/chroma/configure --prefix=${PREFIX} --with-qdp=${QDP_PREFIX} CFLAGS="-O2 -msse -msse2 -march=pentium4" CXXFLAGS="" LIBS="-lgmp" --enable-sse-wilson-dslash  --with-gmp=/usr --enable-testcase-runner=mpirun_rsh  --enable-cg-dwf=sse --with-qmp=${QMP_PREFIX}
