cbc86084b85649a4ef64e9e73c46d213414a561c
edwards <edwards>
2006-11-15 15:57:33 +0000

    Fix.

--- adat/scalar/scalar/build/configure.sh
+++ adat/scalar/scalar/build/configure.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-. ../../../../functions.sh
-PREFIX=`make_prefix ../../../PREFIX ../../../VERSION scalar`
+ADATDIR=../../..
+ROOTDIR=../../../..
+. ${ROOTDIR}/functions.sh
+
+PREFIX=`make_prefix ${ADATDIR}/PREFIX ${ADATDIR}/VERSION scalar`
 clean_dir ${PREFIX}
 
-../../../adat/configure --prefix=${PREFIX}
+${ADATDIR}/adat/configure --prefix=${PREFIX} 
