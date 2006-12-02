be9fcf6791918c89680641b9363066d0b6d7679f
edwards <edwards>
2006-12-02 18:43:41 +0000

    New intel compiler.

--- qdp++/single/parscalar-single-intel/build/configure.sh
+++ qdp++/single/parscalar-single-intel/build/configure.sh
@@ -10,5 +10,6 @@ PREFIX=`make_prefix ${QDPDIR}/PREFIX ${QDPDIR}/VERSION parscalar-${ARCH}-intel`
 clean_dir ${PREFIX}
 
 QMP_PREFIX=`make_prefix ${QMPDIR}/PREFIX ${QMPDIR}/VERSION ${ARCH}-intel`
+CC=/opt/intel/cc/9.1.043/bin/icc
 
-${QDPDIR}/qdp++/configure --prefix=${PREFIX} --with-qmp=${QMP_PREFIX} --enable-parallel-arch=parscalar --enable-sse2 CXX=/opt/intel/cc/9.0/bin/icc CXXFLAGS="-O2 -fast -finline-functions -fno-fnalias -fargument-noalias-global -ftemplate-depth-1000 -msse2 -march=pentium4" CC=/opt/intel/cc/9.0/bin/icc CFLAGS="-O2" LIBS="-static"
+${QDPDIR}/qdp++/configure --prefix=${PREFIX} --with-qmp=${QMP_PREFIX} --enable-parallel-arch=parscalar --enable-sse2 CXX=${CC} CXXFLAGS="-O2 -fast -finline-functions -fno-fnalias -fargument-noalias-global -ftemplate-depth-1000 -msse2 -march=pentium4" CC=${CC} CFLAGS="-O2" LIBS="-static"
