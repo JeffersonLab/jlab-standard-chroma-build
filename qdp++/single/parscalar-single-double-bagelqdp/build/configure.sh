3ea44c6cce16e28a505664185fbda676dedd3ac9
bjoo <bjoo>
2007-02-27 14:37:31 +0000

    PArascalar single bagelqdp build. cb2 layout

---
+++ qdp++/single/parscalar-single-double-bagelqdp/build/configure.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+ARCH=single
+QDPDIR=../../..
+ROOTDIR=../../../..
+QMPDIR=${ROOTDIR}/qmp
+BAGELQDPDIR=${ROOTDIR}/bagel_qdp
+
+. ${ROOTDIR}/functions.sh
+PREFIX=`make_prefix ${QDPDIR}/PREFIX ${QDPDIR}/VERSION parscalar-${ARCH}-double-bagelqdp`
+#clean_dir ${PREFI}
+
+QMP_PREFIX=`make_prefix ${QMPDIR}/PREFIX ${QMPDIR}/VERSION ${ARCH}`
+BAGELQDP_PREFIX=`make_prefix ${BAGELQDPDIR}/PREFIX ${BAGELQDPDIR}/VERSION noarch-double`
+
+${QDPDIR}/qdp++/configure --prefix=${PREFIX} --with-qmp=${QMP_PREFIX} --enable-parallel-arch=parscalar CXXFLAGS="-fargument-noalias-global -O2 -finline-limit=50000" CFLAGS="-O2" --with-bagel-qdp=${BAGELQDP_PREFIX} --enable-precision=double
