c014ef19e9e207d2d2379e561a66dee6d5d0b876
bjoo <bjoo>
2007-02-28 14:24:18 +0000

    Bumped default version numbers for QDP++ and Chroma

--- build_bagel.sh
+++ build_bagel.sh
@@ -1,17 +1,16 @@
 #! /bin/bash
 
-install_root=${HOME}/install
+install_root=${HOME}/Devel/QCD/install
 build_name="Bagel test"
 
 # Build standard versions and install insto standard location
-./configure  \
-  --enable-parallel-make=${parallel_make} \
-  --enable-mailto="${mailto_list}" \
-  --enable-build-name="${build_name}"
+./configure 
+  --enable-install-root=${install_root} \
+  --enable-parallel-make=3
 
 ./build.sh bagel/native/bagel \
 	bagel_qdp/noarch/noarch-double \
-	qmp/ibg2-mpi/ibg2-mpi \
-	bagel_wilson_dslash/ibg2-mpi/parscalar-ibg2-mpi-double \
-	qdp++/ibg2-mpi/parscalar-ibg2-mpi-double-bagelqdp \
-	chroma/ibg2-mpi/parscalar-ibg2-mpi-double-bagel
+	bagel_wilson_dslash/single/parscalar-single-double \
+	qmp/single/single \
+	qdp++/single/parscalar-single-double-bagelqdp \
+	chroma/single/parscalar-single-double-bagel
