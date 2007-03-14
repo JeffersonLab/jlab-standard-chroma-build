52dfb6e9cfff15de0da86b40a87146fe5844247b
bjoo <bjoo>
2007-03-14 13:32:21 +0000

    BGL LibGMP Configure.sh

---
+++ gmp/bgl/bgl/build/configure.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+. ../../../../functions.sh
+PREFIX=`make_prefix ../../../PREFIX ../../../VERSION`
+
+export PATH=/bgl/BlueLight/ppcfloor/blrts-gnu/bin:$PATH
+
+../../../gmp/configure --host=powerpc-none-none --build=none \
+	CC=powerpc-bgl-blrts-gnu-gcc \
+	CXX=powerpc-bgl-blrts-gno-g++ \
+	AR=powerpc-bgl-blrts-gnu-ar \
+	RANLIB=powerpc-bgl-blrts-gnu-ranlib \
+	STRIP=powerpc-bgl-blrts-gnu-strip \
+	CC_FOR_BUILD=gcc \
+     --prefix=${PREFIX} \
+    CFLAGS="-O2" 
