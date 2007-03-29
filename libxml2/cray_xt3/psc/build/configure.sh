dbc20861853b0fa9c4b127cae59cf2b5fbf11cd6
bjoo <bjoo>
2007-03-29 17:44:58 +0000

    Minimal libxml configuration for CrayXT3 at PSC

---
+++ libxml2/cray_xt3/psc/build/configure.sh
@@ -0,0 +1,35 @@
+#! /bin/bash
+
+. ../../../../functions.sh
+PREFIX=`make_prefix ../../../PREFIX ../../../VERSION libxml2`
+clean_dir ${PREFIX}
+
+module unload PrgEnv-pgi
+module load PrgEnv-gnu
+module unload gcc/3.2.3
+module load gcc-catamount/3.3
+
+../../../libxml2/configure --prefix=${PREFIX} -host=x86_64-unknown-linux \
+    --build=x86_64-suse-linux \
+    CC="cc -target=catamount" \
+    CFLAGS="-O2" \
+    --disable-shared \
+    --without-zlib \
+    --without-python \
+    --without-readline \
+    --without-threads \
+    --without-history \
+    --without-reader \
+    --without-writer \
+    --with-output \
+    --without-ftp \
+    --without-http \
+    --without-pattern \
+    --without-catalog \
+    --without-docbook \
+    --without-iconv \
+    --without-schemas \
+    --without-schematron \
+    --without-modules \
+    --without-xptr \
+    --without-xinclude
