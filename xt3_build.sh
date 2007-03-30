13d0f6ce92b77c440482ee90fe43c15ae29f0074
bjoo <bjoo>
2007-03-30 18:37:52 +0000

    New chain

---
+++ xt3_build.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+#### Setup configure here
+./configure --enable-parallel-make=4 \
+#            --enable-install-root = SET_THISUP
+
+./build.sh libxml2/cray_xt3/psc \
+	   gmp/cray_xt3/psc \
+	   qmp/cray_xt3/psc \
+	   qdp++/cray_xt3/parscalar-xt3 \
+	   chroma/cray_xt3/parscalar-xt3
