defb11ba60e334243c69f766fcb611fcf6e48921
bjoo <bjoo>
2007-05-09 17:22:20 +0000

    7n NOAVP build

---
+++ chroma/ib-7n/parscalar-ib-7n-noavp/build/configure.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+ARCH=parscalar-ib-7n
+CHROMADIR=../../..
+ROOTDIR=../../../..
+QDPDIR=${ROOTDIR}/qdp++
+
+. ${ROOTDIR}/functions.sh
+PREFIX=`make_prefix ${CHROMADIR}/PREFIX ${CHROMADIR}/VERSION ${ARCH}-noavp`
+clean_dir ${PREFIX}
+
+QDP_PREFIX=`make_prefix ${QDPDIR}/PREFIX ${QDPDIR}/VERSION ${ARCH}`
+
+${CHROMADIR}/chroma/configure --prefix=${PREFIX} --with-qdp=${QDP_PREFIX} CFLAGS="-O2 -msse -msse2 -march=opteron" CXXFLAGS="-march=opteron" LIBS="-lgmp" --enable-sse-wilson-dslash --with-gmp=/usr --enable-testcase-runner=mpirun_rsh CXX=g++34  CC=gcc34
