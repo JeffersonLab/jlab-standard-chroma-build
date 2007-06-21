813f1783e9125a462256a82e7186f5c5c4a01ce9
bjoo <bjoo>
2007-06-21 18:54:47 +0000

    added bagel_clover build to bgl_build.sh

--- bgl_build.sh
+++ bgl_build.sh
@@ -1,12 +1,18 @@
 #! /bin/bash
 
-./configure --enable-install-root=/project2/teraflop/bjoo/install
+./configure --enable-install-root=/project2/teraflop/bjoo/install \
+	    --enable-bagel-qdp-version=HEAD \
+	    --enable-qdp-version=HEAD \
+	    --enable-chroma-version=HEAD \
+	    --enable-parallel-make=4
 
-./build.sh libxml2/bgl/bgl \
+#./build.sh \
+          libxml2/bgl/bgl \
 	   gmp/bgl/bgl \
            bagel/native/bagel \
 	   bagel_qdp/bgl/bgl-double \
 	   qmp/bgl/bgl \
-	   bagel_wilson_dslash/bgl/parscalar-bgl-double \
-	   qdp++/bgl/parscalar-bgl-double \
+	   qdp++/bgl/parscalar-bgl-double   \
+           bagel_wilson_dslash/bgl/parscalar-bgl-double \
+	   bagel_clover/bgl/bgl-double \
 	   chroma/bgl/parscalar-bgl-double
