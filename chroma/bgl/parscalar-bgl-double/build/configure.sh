05f7030758b074c79c0db72b1b5d1c1003f25fe0
bjoo <bjoo>
2007-06-21 18:55:29 +0000

    Added BAGEL Clover to this

--- chroma/bgl/parscalar-bgl-double/build/configure.sh
+++ chroma/bgl/parscalar-bgl-double/build/configure.sh
@@ -4,12 +4,14 @@ ARCH=${BASEARCH}
 QDPARCH=${BASEARCH}
 BWDARCH=${BASEARCH}
 GMPARCH=bgl
+CLOVARCH=bgl-double
 
 CHROMADIR=../../..
 ROOTDIR=../../../..
 QDPDIR=${ROOTDIR}/qdp++
 BWDDIR=${ROOTDIR}/bagel_wilson_dslash
 GMPDIR=${ROOTDIR}/gmp
+CLOVDIR=${ROOTDIR}/bagel_clover
 . ${ROOTDIR}/functions.sh
 PREFIX=`make_prefix ${CHROMADIR}/PREFIX ${CHROMADIR}/VERSION ${ARCH}-sloppy`
 clean_dir ${PREFIX}
@@ -17,18 +19,21 @@ clean_dir ${PREFIX}
 QDP_PREFIX=`make_prefix ${QDPDIR}/PREFIX ${QDPDIR}/VERSION ${QDPARCH}`
 BWD_PREFIX=`make_prefix ${BWDDIR}/PREFIX ${BWDDIR}/VERSION ${BWDARCH}`
 GMP_PREFIX=`make_prefix ${GMPDIR}/PREFIX ${GMPDIR}/VERSION ${GMPARCH}` 
+CLOV_PREFIX=`make_prefix ${CLOVDIR}/PREFIX ${CLOVDIR}/VERSION ${CLOVARCH}`
 echo $QDP_PREFIX
 echo $GMP_PREFIX
 
 echo ${BWD_PREFIX}
+echo ${CLOV_PREFIX}
 export PATH=/bgl/BlueLight/ppcfloor/blrts-gnu/bin:$PATH
 
 ${CHROMADIR}/chroma/configure --prefix=${PREFIX} \
 			     --host=powerpc-bgl-blrts-gnu \
 			     --build=none \
 			     --with-qdp=${QDP_PREFIX} \
-			     --enable-testcase-runner=cobalt \
+			     --enable-testcase-runner=load_leveller \
 			     CFLAGS="-O2" CXXFLAGS="" LIBS="" \
 	 --with-bagel-wilson-dslash=${BWD_PREFIX} \
 	 --enable-bagel-wilson-dslash-sloppy=yes \
-	 --with-gmp=${GMP_PREFIX}
+	 --with-gmp=${GMP_PREFIX} \
+         --with-bagel-clover=${CLOV_PREFIX}
