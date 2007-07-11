6a95e7cd1f0c2f8b9ba54ec3f5ed06edbe1e77db
bjoo <bjoo>
2007-07-11 21:04:56 +0000

    MOdified for OFED1.2

--- qmp/ib-7n/ib-7n/build/configure.sh
+++ qmp/ib-7n/ib-7n/build/configure.sh
@@ -4,9 +4,8 @@
 PREFIX=`make_prefix ../../../PREFIX ../../../VERSION ib-7n`
 clean_dir ${PREFIX}
 
-export MPICH_HOME=/usr/local/mvapich-0.9.9
-
+#export MPICH_HOME=/usr/local/mvapich-0.9.9
+export MPICH_HOME=/usr/local/ofed1.2/mpi/gcc/mvapich-0.9.9
 ../../../qmp/configure --prefix=${PREFIX} --with-qmp-comms-type=MPI \
     --with-qmp-comms-cflags=" -I${MPICH_HOME}/include -DUSE_STDARG -DHAVE_STDLIB_H=1 -DHAVE_STRING=1 -DHAVE_UNISTD_H=1 -DHAVE_STDARG_H=1 -DUSE_STDARG=1 -DMALLOC_RET_VOID=1" \
-    --with-qmp-comms-ldflags="-L${MPICH_HOME}/lib -L/usr/local/ofed/lib64 -Wl,-rpath=/usr/local/ofed/lib64" \
-    --with-qmp-comms-libs="-lmpich -libcommon -libverbs -libumad -lpthread" CFLAGS="-O2" CC=gcc34
+    --with-qmp-comms-ldflags="-Wl,-rpath-link -Wl,/usr/local/ofed1.2/mpi/gcc/mvapich-0.9.9/lib/shared -L/usr/local/ofed1.2/mpi/gcc/mvapich-0.9.9/lib/shared -L/usr/local/ofed1.2/mpi/gcc/mvapich-0.9.9/lib  -L/usr/local/ofed1.2/lib64" --with-qmp-comms-libs="-lmpich -libverbs -libumad -libcommon -lpthread -Wall -lpthread -lrt" CFLAGS="-O2" CC=gcc34
