461422234610d829fefd443cf6cc91704b4e4f1c
bjoo <bjoo>
2007-08-23 12:16:25 +0000

    Set up Intel compiler on qcdi02

--- qmp/single/single-intel/build/configure.sh
+++ qmp/single/single-intel/build/configure.sh
@@ -6,4 +6,4 @@ clean_dir ${PREFIX}
 
 CC=/opt/intel/cc/9.1.043/bin/icc
 
-../../../qmp/configure --prefix=${PREFIX} --with-qmp-comms-type=SINGLE CC=${CC} LIBS="-static"
+../../../qmp/configure --prefix=${PREFIX} --with-qmp-comms-type=SINGLE CC=${CC} CFLAGS="-O2 -march=pentium4"
