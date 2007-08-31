a9bcfc6c43ff045e2f7d8fc690d01b6d39515347
bjoo <bjoo>
2007-08-31 18:54:03 +0000

    Changed this to gcc4 for nightlies

--- qdp++/gigE/parscalar-gigE/build/configure.sh
+++ qdp++/gigE/parscalar-gigE/build/configure.sh
@@ -8,4 +8,7 @@ ROOTDIR=../../../..
 PREFIX=`make_prefix ${QDPDIR}/PREFIX ${QDPDIR}/VERSION ${ARCH}`
 clean_dir ${PREFIX}
 
-${QDPDIR}/qdp++/configure --prefix=${PREFIX} --with-qmp=/usr/local/qmp/mvia --enable-parallel-arch=parscalar --enable-sse2 CXXFLAGS="-O2 -finline-limit=50000 -msse -msse2 -march=pentium4" CFLAGS="-O3 -msse -msse2 -march=pentium4"
+CXX=/dist/gcc-4.1.1/bin/g++
+CC=/dist/gcc-4.1.1/bin/gcc
+
+${QDPDIR}/qdp++/configure --prefix=${PREFIX} --with-qmp=/usr/local/qmp/mvia --enable-parallel-arch=parscalar --enable-sse2 CXXFLAGS="-O2 -finline-limit=50000 -msse -msse2 -march=pentium4" CFLAGS="-O3 -msse -msse2 -march=pentium4" CXX=${CXX} CC=${CC}
