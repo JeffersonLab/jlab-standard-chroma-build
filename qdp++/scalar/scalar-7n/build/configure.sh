ca04b6c12250e219de8a04566ceba686bad606e7
bjoo <bjoo>
2007-08-31 19:06:01 +0000

    Moved this up to gcc-4

--- qdp++/scalar/scalar-7n/build/configure.sh
+++ qdp++/scalar/scalar-7n/build/configure.sh
@@ -7,4 +7,4 @@ ROOTDIR=../../../..
 PREFIX=`make_prefix ${QDPDIR}/PREFIX ${QDPDIR}/VERSION scalar-7n`
 clean_dir ${PREFIX}
 
-${QDPDIR}/qdp++/configure --prefix=${PREFIX} --enable-parallel-arch=scalar --enable-sse2 CXXFLAGS="-fargument-noalias-global -O2 -finline-limit=50000 -msse2 -march=opteron -m64" CFLAGS="-fargument-noalias-global -O2 -msse -msse2 -march=opteron -m64" CXX=g++34 CC=gcc34
+${QDPDIR}/qdp++/configure --prefix=${PREFIX} --enable-parallel-arch=scalar --enable-sse2 CXXFLAGS="-fargument-noalias-global -O2 -finline-limit=50000 -msse2 -march=opteron -m64" CFLAGS="-fargument-noalias-global -O2 -msse -msse2 -march=opteron -m64" CXX=g++ CC=gcc
