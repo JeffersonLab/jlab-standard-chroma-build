14b5fa05760edc7c03010b6dde84b7c8ec318707
bjoo <bjoo>
2007-08-31 18:56:54 +0000

    Configure change - in case we do regressions with this

--- chroma/gigE/parscalar-gigE/build/configure.sh
+++ chroma/gigE/parscalar-gigE/build/configure.sh
@@ -12,4 +12,7 @@ clean_dir ${PREFIX}
 QDP_PREFIX=`make_prefix ${QDPDIR}/PREFIX ${QDPDIR}/VERSION ${ARCH}`
 QMP_PREFIX=/usr/local/qmp/mvia
 
-${CHROMADIR}/chroma/configure --prefix=${PREFIX} --with-qdp=${QDP_PREFIX} CFLAGS="-O2 -msse -msse2 -march=pentium4" CXXFLAGS="" LIBS="-lgmp" --enable-sse-wilson-dslash  --with-gmp=/usr --enable-cg-dwf=sse --with-qmp=${QMP_PREFIX}
+CC=/dist/gcc-4.1.1/bin/gcc
+CXX=/dist/gcc-4.1.1/bin/g++
+
+${CHROMADIR}/chroma/configure --prefix=${PREFIX} --with-qdp=${QDP_PREFIX} CFLAGS="-O2 -msse -msse2 -march=pentium4" CXXFLAGS="" LIBS="-lgmp" --enable-sse-wilson-dslash  --with-gmp=/usr --enable-cg-dwf=sse --with-qmp=${QMP_PREFIX} CC=${CC} CXX=${CXX}
