26c77498a000bb926903052e1665ce041a99c379
edwards <edwards>
2007-09-10 13:40:38 +0000

    Changed to use gcc4

--- adat/scalar/scalar/build/configure.sh
+++ adat/scalar/scalar/build/configure.sh
@@ -4,7 +4,10 @@ ADATDIR=../../..
 ROOTDIR=../../../..
 . ${ROOTDIR}/functions.sh
 
+CC=/dist/gcc-4.1.1/bin/gcc
+CXX=/dist/gcc-4.1.1/bin/g++
+
 PREFIX=`make_prefix ${ADATDIR}/PREFIX ${ADATDIR}/VERSION scalar`
 clean_dir ${PREFIX}
 
-${ADATDIR}/adat/configure --prefix=${PREFIX} 
+${ADATDIR}/adat/configure --prefix=${PREFIX} CXX=${CXX} CC=${CC}
