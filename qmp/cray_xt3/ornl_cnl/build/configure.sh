52e92d5cede0bfc2a4006b3d49f34c88abc3924a
bjoo <bjoo>
2007-11-05 15:10:24 +0000

    New Flags

--- qmp/cray_xt3/ornl_cnl/build/configure.sh
+++ qmp/cray_xt3/ornl_cnl/build/configure.sh
@@ -4,16 +4,11 @@
 PREFIX=`make_prefix ../../../PREFIX ../../../VERSION`
 clean_dir ${PREFIX}
 
-module unload PrgEnv-pgi
-module load PrgEnv-gnu
-module unload gcc/3.2.3
-module load gcc/3.3.3
-
 ../../../qmp/configure --host=x86_64-unknown-linux-gnu --build=x86_64-suse-linux \
     --with-qmp-comms-type=MPI \
     --with-qmp-comms-cflags="" \
     --with-qmp-comms-ldflags="" \
     --with-qmp-comms-libs="" \
     --prefix=${PREFIX} \
-    CC="/opt/xt-pe/${XTOS_VERSION}/bin/snos64/cc" \
-    CFLAGS="-O2" 
+    CC="cc" \
+    CFLAGS="-O3 -march=opteron" 
