f6b205af8a79026bcf99345a13597197267f92f8
bjoo <bjoo>
2007-11-05 15:35:20 +0000

    New Flags

--- qdp++/cray_xt3/parscalar-xt3/build/configure.sh
+++ qdp++/cray_xt3/parscalar-xt3/build/configure.sh
@@ -11,5 +11,5 @@ clean_dir ${PREFIX}
 QMP_PREFIX=`make_prefix ${ROOTDIR}/qmp/PREFIX ${ROOTDIR}/qmp/VERSION`
 LIBXML_PREFIX=`make_prefix ${ROOTDIR}/libxml2/PREFIX ${ROOTDIR}/libxml2/VERSION libxml2`
 
-${QDPDIR}/qdp++/configure --prefix=${PREFIX} --with-qmp=${QMP_PREFIX} --enable-parallel-arch=parscalar --enable-sse2 CXXFLAGS="-fargument-noalias-global -O3 -finline-limit=50000 -msse -msse2 -march=opteron" CFLAGS="-fargument-noalias-global -O3 -msse -msse2 -march=opteron" --enable-largefile  \
+${QDPDIR}/qdp++/configure --prefix=${PREFIX} --with-qmp=${QMP_PREFIX} --enable-parallel-arch=parscalar --enable-sse2 CXXFLAGS="-O3 -fargument-noalias-global -finline-limit=50000 -funroll-all-loops -fpeel-loops -ftree-vectorize -march=opteron -mtune=opteron" CFLAGS="-O3 -fargument-noalias-global -funroll-all-loops -fpeel-loops -ftree-vectorize -march=opteron -mtune=opteron" --enable-largefile  \
 CC="cc" CXX="CC" --host=x86_64-unknown-linux-gnu --build=x86_64-suse-linux --with-libxml2=${LIBXML_PREFIX}
