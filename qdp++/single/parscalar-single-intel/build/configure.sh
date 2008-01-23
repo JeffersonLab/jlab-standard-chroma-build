1c097d4fda3ddbea1a3cf0f574c50b969f9c20eb
edwards <edwards>
2008-01-23 14:46:17 +0000

    Changed to use new compiler paths.

--- qdp++/single/parscalar-single-intel/build/configure.sh
+++ qdp++/single/parscalar-single-intel/build/configure.sh
@@ -10,8 +10,15 @@ PREFIX=`make_prefix ${QDPDIR}/PREFIX ${QDPDIR}/VERSION parscalar-${ARCH}-intel`
 clean_dir ${PREFIX}
 
 QMP_PREFIX=`make_prefix ${QMPDIR}/PREFIX ${QMPDIR}/VERSION ${ARCH}-intel`
-CC=/opt/intel/cc/9.1.043/bin/icc
-CXX=/opt/intel/cc/9.1.043/bin/icpc
+
+CC=/opt/intel/cce/10.0.023/bin/icc
+CXX=/opt/intel/cce/10.0.023/bin/icpc
 
 #export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/intel/cc/9.1.043/lib
-${QDPDIR}/qdp++/configure --prefix=${PREFIX} --with-qmp=${QMP_PREFIX} --enable-parallel-arch=parscalar CXX=${CXX} CC=${CC} CXXFLAGS="-O2 -finline-limit=50000  -fargument-noalias-global -march=pentium4" CC=${CC} CFLAGS="-O2 -march=pentium4" LIBS=""
+
+${QDPDIR}/qdp++/configure --prefix=${PREFIX} --with-qmp=${QMP_PREFIX} \
+   --enable-parallel-arch=parscalar \
+   CXXFLAGS="-O2 -finline-limit=50000  -fargument-noalias-global -march=pentium4" \
+   CFLAGS="-O2 -march=pentium4" \
+   LIBS="" \
+   CXX=${CXX} CC=${CC}
