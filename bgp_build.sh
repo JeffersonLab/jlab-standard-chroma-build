ad57e24872070d47a5cd55e217f451b297427a4a
bjoo <bjoo>
2008-04-16 13:22:20 +0000

    BGP Build Script

---
+++ bgp_build.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+./configure --enable-install-root=$HOME/install \
+	    --enable-bagel-qdp-version=HEAD \
+	    --enable-qdp-version=HEAD \
+	    --enable-chroma-version=HEAD \
+	    --enable-parallel-make=4
+
+./build.sh \
+          libxml2/bgp/bgp \
+	   gmp/bgp/bgp \
+           bagel/native/bagel \
+	   bagel_qdp/bgp/bgp-double \
+	   qmp/bgp/bgp \
+	   qdp++/bgp/parscalar-bgp-double   \
+           bagel_wilson_dslash/bgp/parscalar-bgp-double \
+	   bagel_clover/bgp/bgp-double \
+	   chroma/bgp/parscalar-bgp-double-sloppy
