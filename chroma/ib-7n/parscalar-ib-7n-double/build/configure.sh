895df875eae61bccb55954ac0b33ad763ef3764b
bjoo <bjoo>
2008-04-23 14:19:31 +0000

    Configure.sh fixed

--- chroma/ib-7n/parscalar-ib-7n-double/build/configure.sh
+++ chroma/ib-7n/parscalar-ib-7n-double/build/configure.sh
@@ -13,13 +13,16 @@ clean_dir ${PREFIX}
 QDP_PREFIX=`make_prefix ${QDPDIR}/PREFIX ${QDPDIR}/VERSION ${ARCH}`
 QMP_PREFIX=`make_prefix ${QMPDIR}/PREFIX ${QMPDIR}/VERSION ib-7n`
 
+MPICH_HOME=/usr/local/mvapich-0.9.9
+CC=${MPICH_HOME}/bin/mpicc
+CXX=${MPICH_HOME}/bin/mpicxx
 
 ${CHROMADIR}/chroma/configure --prefix=${PREFIX} --with-qdp=${QDP_PREFIX} \
   CFLAGS="-O2 -msse -msse2 -march=opteron -m64" \
   CXXFLAGS="-march=opteron -m64" \
   --enable-sse-wilson-dslash --with-gmp=/usr --enable-testcase-runner=6n_mpirun_rsh \
   --enable-cg-dwf=sse --with-qmp=${QMP_PREFIX} \
-  LIBS="-lgmp"
+  LIBS="-lgmp" CC=${CC} CXX=${CXX}
 
 #  --enable-lapack=lapack LIBS="-llapack -lblas -lgfortran -lgmp"
 
