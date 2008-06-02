8addc831083c21eed27ae77ec133de17bd051d68
edwards <edwards>
2008-06-02 15:27:20 +0000

    Updated.

--- chroma/ib-7n/parscalar-ib-7n-noavp/build/configure.sh
+++ chroma/ib-7n/parscalar-ib-7n-noavp/build/configure.sh
@@ -4,17 +4,17 @@ ARCH=parscalar-ib-7n
 CHROMADIR=../../..
 ROOTDIR=../../../..
 QDPDIR=${ROOTDIR}/qdp++
-
+QMPDIR=${ROOTDIR}/qmp
 . ${ROOTDIR}/functions.sh
 PREFIX=`make_prefix ${CHROMADIR}/PREFIX ${CHROMADIR}/VERSION ${ARCH}-noavp`
 clean_dir ${PREFIX}
 
 QDP_PREFIX=`make_prefix ${QDPDIR}/PREFIX ${QDPDIR}/VERSION ${ARCH}`
+QMP_PREFIX=`make_prefix ${QMPDIR}/PREFIX ${QMPDIR}/VERSION ib-7n`
 
+export MPICH_HOME=/usr/local/mvapich-0.9.9
 ${CHROMADIR}/chroma/configure --prefix=${PREFIX} --with-qdp=${QDP_PREFIX} \
-  CFLAGS="-O2 -msse -msse2 -march=opteron" \
-  CXXFLAGS="-march=opteron" \
-  LIBS="-lgmp" \
-  --enable-sse-wilson-dslash --with-gmp=/usr \
-  --enable-testcase-runner=6n_mpirun_rsh \
-  CXX=g++  CC=gcc
+   CFLAGS="-O3 -march=opteron -funroll-loops -fpeel-loops" CXXFLAGS="" \
+   --enable-sse-wilson-dslash  --with-gmp=/usr --enable-testcase-runner=6n_mpirun_rsh  \
+   --with-qmp=${QMP_PREFIX} CC=${MPICH_HOME}/bin/mpicc  CXX=${MPICH_HOME}/bin/mpicxx \
+   --enable-lapack=lapack LIBS="-llapack -lblas -lgfortran -lgmp"
