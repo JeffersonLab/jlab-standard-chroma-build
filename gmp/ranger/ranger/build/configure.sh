0812d3c760b8195890818e5f52240acd77f9ca07
bjoo <bjoo>
2008-06-03 14:10:03 +0000

    Changed this to use my gcc

--- gmp/ranger/ranger/build/configure.sh
+++ gmp/ranger/ranger/build/configure.sh
@@ -4,9 +4,10 @@
 PREFIX=`make_prefix ../../../PREFIX ../../../VERSION ib`
 
 ../../../gmp/configure --host=x86_64-unknown-linux --build=x86_64-suse-linux \
-	CC="$HOME/gnu/bin/gcc" \
-	CXX="$HOME/gnu/bin/g++" \
-	CC_FOR_BUILD="$HOME/gnu/bin/gcc" \
+	CC="mpicc -cc=$HOME/install/gcc-4.2.3/bin/gcc" \
+	CXX="mpiCC -CC=$HOME/install/gcc-4.2.3/bin/g++" \
+	CC_FOR_BUILD="gcc" \
      --prefix=${PREFIX} \
      --disable-shared \
-    CFLAGS="-O3" 
+    CFLAGS="-O3 -funroll-all-loops -fpeel-loops -ftree-vectorize -march=opteron"   CXXFLAGS="-03  -funroll-all-loops -fpeel-loops -ftree-vectorize -march=opteron "
+
