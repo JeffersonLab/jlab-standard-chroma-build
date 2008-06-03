f875cdfca6328d77a85c42de0974dd294f8d5dc9
bjoo <bjoo>
2008-06-03 14:11:44 +0000

    Changed compiler

--- qmp/ranger/ranger/build/configure.sh
+++ qmp/ranger/ranger/build/configure.sh
@@ -5,4 +5,4 @@ PREFIX=`make_prefix ../../../PREFIX ../../../VERSION ib-7n`
 clean_dir ${PREFIX}
 
 
-../../../qmp/configure --prefix=${PREFIX} --with-qmp-comms-type=MPI --with-qmp-comms-cflags="" --with-qmp-comms-ldflags="" --with-qmp-comms-libs="" CC="mpicc -cc=$HOME/gnu/bin/gcc" CFLAGS="-O3 -march=opteron " --host=x86_64-linux-gnu --build=none
+../../../qmp/configure --prefix=${PREFIX} --with-qmp-comms-type=MPI --with-qmp-comms-cflags="" --with-qmp-comms-ldflags="" --with-qmp-comms-libs="" CC="mpicc -cc=$HOME/install/gcc-4.2.3/bin/gcc" CFLAGS="-O3 -funroll-all-loops -fpeel-loops -ftree-vectorize -march=opteron" --host=x86_64-linux-gnu --build=none
