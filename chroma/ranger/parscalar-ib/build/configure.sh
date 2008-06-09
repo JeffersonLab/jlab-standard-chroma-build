681472b3072913cfd757c8075f0416cc059a3a50
edwards <edwards>
2008-06-09 03:34:34 +0000

    Changed to use opteigcg which means it now needs lapack - which here is acml.

--- chroma/ranger/parscalar-ib/build/configure.sh
+++ chroma/ranger/parscalar-ib/build/configure.sh
@@ -17,9 +17,15 @@ GMP_PREFIX=`make_prefix ${GMPDIR}/PREFIX ${GMPDIR}/VERSION ib`
 
 TOOLS=/share/home/00496/tg457586
 
+STUFF=/share/apps/intel10_1/acml/4.0.1/ifort64/lib
+
 ${CHROMADIR}/chroma/configure --prefix=${PREFIX} --with-qdp=${QDP_PREFIX} \
    CFLAGS="-O3 -funroll-all-loops -fpeel-loops -ftree-vectorize -fno-tree-vrp " CXXFLAGS="" LIBS="" \
    --enable-sse-wilson-dslash --with-gmp=$GMP_PREFIX --enable-testcase-runner=6n_mpirun_rsh  \
    --with-qmp=${QMP_PREFIX} \
-CXX="mpicxx -CC=$TOOLS/install/gcc-4.2.3/bin/g++" \
-CC="mpicc -cc=$TOOLS/install/gcc-4.2.3/bin/gcc"
+   --enable-opt-eigcg --enable-lapack=lapack LIBS="$STUFF/libacml.a -lgmp" \
+   LDFLAGS="-L$HOME/install/gmp/4.2.1/ib/lib" \
+   CXX="mpicxx -CC=$TOOLS/install/gcc-4.2.3/bin/g++" \
+   CC="mpicc -cc=$TOOLS/install/gcc-4.2.3/bin/gcc"
+
+#   --enable-opt-eigcg --enable-lapack=lapack LIBS="-llapack -lblas -lgfortran -lgmp" \
