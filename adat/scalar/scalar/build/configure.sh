c5cf09ee589b8eb66f2e146833d49b3ba2aa4550
edwards <edwards>
2008-08-06 17:14:26 +0000

    Added --enable-db-lite

--- adat/scalar/scalar/build/configure.sh
+++ adat/scalar/scalar/build/configure.sh
@@ -10,4 +10,5 @@ CXX=g++
 PREFIX=`make_prefix ${ADATDIR}/PREFIX ${ADATDIR}/VERSION scalar`
 clean_dir ${PREFIX}
 
-${ADATDIR}/adat/configure --prefix=${PREFIX} CXX=${CXX} CC=${CC}
+${ADATDIR}/adat/configure --prefix=${PREFIX} CXX=${CXX} CC=${CC} \
+   --enable-db-lite 
