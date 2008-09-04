3ab2d06f0fcf5c07c7febee2d17505895ff542d4
edwards <edwards>
2008-09-04 20:54:49 +0000

    Added inline-limit flag.

--- adat/scalar/scalar/build/configure.sh
+++ adat/scalar/scalar/build/configure.sh
@@ -11,6 +11,6 @@ PREFIX=`make_prefix ${ADATDIR}/PREFIX ${ADATDIR}/VERSION scalar`
 clean_dir ${PREFIX}
 
 ${ADATDIR}/adat/configure --prefix=${PREFIX} \
-   CFLAGS="-O3 -fargument-noalias-global -funroll-all-loops -fpeel-loops -march=opteron" \
-   CXXFLAGS="-O3 -fargument-noalias-global -funroll-all-loops -fpeel-loops -march=opteron" \
+   CFLAGS="-O3 -fargument-noalias-global -finline-limit=50000 -funroll-all-loops -fpeel-loops -march=opteron" \
+   CXXFLAGS="-O3 -fargument-noalias-global -finline-limit=50000 -funroll-all-loops -fpeel-loops -march=opteron" \
    --enable-db-lite 
