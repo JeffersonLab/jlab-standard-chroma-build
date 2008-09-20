2b3f282c66891a7a925ebc913ed2ddf52d8c03cf
edwards <edwards>
2008-09-20 18:15:59 +0000

    Copied over from single-prec.

--- chroma/scalar/scalar-double/build/configure.sh
+++ chroma/scalar/scalar-double/build/configure.sh
@@ -5,16 +5,16 @@ CHROMADIR=../../..
 ROOTDIR=../../../..
 QDPDIR=${ROOTDIR}/qdp++
 
-CC=gcc
-CXX=g++
-
 . ${ROOTDIR}/functions.sh
 PREFIX=`make_prefix ${CHROMADIR}/PREFIX ${CHROMADIR}/VERSION ${ARCH}`
 clean_dir ${PREFIX}
 
 QDP_PREFIX=`make_prefix ${QDPDIR}/PREFIX ${QDPDIR}/VERSION ${ARCH}`
 
-${CHROMADIR}/chroma/configure --prefix=${PREFIX} --with-qdp=${QDP_PREFIX} CFLAGS="-O3 -msse -msse2 -march=nocona" \
-   CXXFLAGS="" --enable-sse-wilson-dslash  --with-gmp=/usr CC=${CC} CXX=${CXX} \
-   --enable-lapack=lapack LIBS="-llapack -lblas -lgfortran -lgmp"
+
+${CHROMADIR}/chroma/configure --prefix=${PREFIX} --with-qdp=${QDP_PREFIX} \
+	CFLAGS="-O3 -fargument-noalias-global -funroll-all-loops -fpeel-loops -march=opteron" \
+	CXXFLAGS="" \
+        --enable-sse-wilson-dslash --with-gmp=/usr CC=gcc CXX=g++ 
+
 
--- qdp++/scalar/scalar-double/build/configure.sh
+++ qdp++/scalar/scalar-double/build/configure.sh
@@ -4,10 +4,11 @@ QDPDIR=../../..
 ROOTDIR=../../../..
 . ${ROOTDIR}/functions.sh
 
-CC=/dist/gcc-4.1.1/bin/gcc
-CXX=/dist/gcc-4.1.1/bin/g++
-
 PREFIX=`make_prefix ${QDPDIR}/PREFIX ${QDPDIR}/VERSION scalar-double`
 clean_dir ${PREFIX}
 
-${QDPDIR}/qdp++/configure --prefix=${PREFIX} --enable-parallel-arch=scalar --enable-sse2 CXX=${CXX} CXXFLAGS="-O2 -finline-limit=50000 -msse -march=pentium4" CC=${CC} CFLAGS="-O2" --enable-precision=double
+${QDPDIR}/qdp++/configure --prefix=${PREFIX} --enable-parallel-arch=scalar --enable-sse2 --enable-db-lite \
+	CXXFLAGS="-fargument-noalias-global -O3 -finline-limit=50000 -march=opteron -funroll-all-loops -fpeel-loops" \
+	CFLAGS="-fargument-noalias-global -O3 -march=opteron -funroll-all-loops -fpeel-loops" \
+	CXX=g++ CC=gcc \
+        --enable-precision=double
