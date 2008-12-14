80ec18d8ffc09fa2ec3022f9c760c95afafcbb9e
bjoo <bjoo>
2008-12-14 10:01:20 +0000

    Configure for QMT

---
+++ qmt/intel/intel/build/configure.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+QMTDIR=../../..
+ROOTDIR=../../../..
+. ${ROOTDIR}/functions.sh
+
+PREFIX=`make_prefix ${QMTDIR}/PREFIX ${QMTDIR}/VERSION intel`
+clean_dir ${PREFIX}
+
+${QMTDIR}/qmt/configure --prefix=${PREFIX} --enable-xeon \
+	--disable-thread_affinity \
+	CFLAGS="-O3 -D_REENTRANT" \
+	CXX=g++ CC=gcc
