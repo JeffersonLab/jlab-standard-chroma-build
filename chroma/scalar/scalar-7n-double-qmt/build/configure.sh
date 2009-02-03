d906b601867f48859e68c1f45292c50cb08e4c41
bjoo <bjoo>
2009-02-03 21:15:35 +0000

    Added  threaded Chroma

---
+++ chroma/scalar/scalar-7n-double-qmt/build/configure.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+ARCH=scalar-7n-double
+CHROMADIR=../../..
+ROOTDIR=../../../..
+QDPDIR=${ROOTDIR}/qdp++
+QMTDIR=${ROOTDIR}/qmt
+
+. ${ROOTDIR}/functions.sh
+PREFIX=`make_prefix ${CHROMADIR}/PREFIX ${CHROMADIR}/VERSION ${ARCH}`
+clean_dir ${PREFIX}
+
+QDP_PREFIX=`make_prefix ${QDPDIR}/PREFIX ${QDPDIR}/VERSION ${ARCH}-qmt`
+QMT_PREFIX=`make_prefix ${QMTDIR}/PREFIX ${QMTDIR}/VERSION amd`
+${CHROMADIR}/chroma/configure --prefix=${PREFIX} --with-qdp=${QDP_PREFIX} \
+	CFLAGS="-O3 -fargument-noalias-global -funroll-all-loops -fpeel-loops -march=opteron" \
+	CXXFLAGS="" \
+        --enable-sse-wilson-dslash --with-gmp=/usr CC=gcc CXX=g++  \
+	--with-qmt=$QMT_PREFIX \
+	--enable-ssed-clover
+
+
