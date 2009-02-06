5ea3e9a6c645124b8f6e5315834a37c6f11e52a1
bjoo <bjoo>
2009-02-06 16:38:24 +0000

    Single prec with QMT configure

---
+++ qdp++/cray_xt3/parscalar-xt-qmt/build/configure.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+ARCH=xt
+QDPDIR=../../..
+ROOTDIR=../../../..
+QMTDIR=${ROOTDIR}/qmt
+
+. ${ROOTDIR}/functions.sh
+
+PREFIX=`make_prefix ${QDPDIR}/PREFIX ${QDPDIR}/VERSION parscalar-${ARCH}-qmt`
+clean_dir ${PREFIX}
+
+QMP_PREFIX=`make_prefix ${ROOTDIR}/qmp/PREFIX ${ROOTDIR}/qmp/VERSION`
+QMT_PREFIX=`make_prefix ${QMTDIR}/PREFIX ${QMTDIR}/VERSION xt-barcelona`
+LIBXML_PREFIX=`make_prefix ${ROOTDIR}/libxml2/PREFIX ${ROOTDIR}/libxml2/VERSION libxml2`
+
+${QDPDIR}/qdp++/configure --prefix=${PREFIX} --with-qmp=${QMP_PREFIX} --enable-parallel-arch=parscalar --enable-sse2 CXXFLAGS="-O3 -fargument-noalias-global -finline-limit=50000 -funroll-all-loops -fpeel-loops -ftree-vectorize" CFLAGS="-O3 -fargument-noalias-global -funroll-all-loops -fpeel-loops -ftree-vectorize" --enable-largefile  \
+CC="cc" CXX="CC" --host=x86_64-unknown-linux-gnu --build=x86_64-suse-linux --with-libxml2=${LIBXML_PREFIX} --with-qmt=${QMT_PREFIX}
