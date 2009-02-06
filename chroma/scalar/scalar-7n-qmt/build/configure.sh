8932a4dada3d701c7b6d1e222144a7c1c0f7fc6c
bjoo <bjoo>
2009-02-06 19:26:43 +0000

    Changed Prefix

--- chroma/scalar/scalar-7n-qmt/build/configure.sh
+++ chroma/scalar/scalar-7n-qmt/build/configure.sh
@@ -7,7 +7,7 @@ QDPDIR=${ROOTDIR}/qdp++
 QMTDIR=${ROOTDIR}/qmt
 
 . ${ROOTDIR}/functions.sh
-PREFIX=`make_prefix ${CHROMADIR}/PREFIX ${CHROMADIR}/VERSION ${ARCH}`
+PREFIX=`make_prefix ${CHROMADIR}/PREFIX ${CHROMADIR}/VERSION ${ARCH}-qmt`
 clean_dir ${PREFIX}
 
 QDP_PREFIX=`make_prefix ${QDPDIR}/PREFIX ${QDPDIR}/VERSION ${ARCH}-qmt`
