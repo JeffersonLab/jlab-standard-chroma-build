ae9af8d6d7b75d3254d3ee289c6eb8038237423b
bjoo <bjoo>
2009-02-13 19:58:38 +0000

    ./configure for a PAT build

---
+++ gmp/cray_xt3/xt-craypat/build/configure.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+. ../../../../functions.sh
+PREFIX=`make_prefix ../../../PREFIX ../../../VERSION xt-pat`
+
+../../../gmp/configure --host=x86_64-unknown-linux --build=x86_64-suse-linux \
+	CC="cc" \
+	CXX="CC" \
+	CC_FOR_BUILD=gcc \
+     --prefix=${PREFIX} \
+     --disable-shared \
+    CFLAGS="-O2 -msse -msse2 -march=barcelona -g" 
