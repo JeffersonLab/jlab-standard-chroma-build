e36622c216e2c35821ac8f955071b54c86aa468a
bjoo <bjoo>
2009-02-27 15:17:33 +0000

    PAT configure

---
+++ qmp/cray_xt3/xt-craypat/build/configure.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+. ../../../../functions.sh
+PREFIX=`make_prefix ../../../PREFIX ../../../VERSION xt-pat`
+clean_dir ${PREFIX}
+
+../../../qmp/configure --host=x86_64-unknown-linux-gnu --build=x86_64-suse-linux \
+    --with-qmp-comms-type=MPI \
+    --with-qmp-comms-cflags="" \
+    --with-qmp-comms-ldflags="" \
+    --with-qmp-comms-libs="" \
+     --prefix=${PREFIX} \
+    CC="cc" \
+    CFLAGS="-O3 -msse -msse2 -march=barcelona" 
