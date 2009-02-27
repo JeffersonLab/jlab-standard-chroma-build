cc250eb5235bc8fb414f5f4b4e39f474c7b95a25
bjoo <bjoo>
2009-02-27 15:24:06 +0000

    PAT build

---
+++ chroma/cray_xt3/parscalar-xt-qmt-pat/build/configure.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+BASEARCH=parscalar-xt
+ARCH=${BASEARCH}
+QDPARCH=${BASEARCH}
+GMPARCH=xt-pat
+
+CHROMADIR=../../..
+ROOTDIR=../../../..
+QDPDIR=${ROOTDIR}/qdp++
+GMPDIR=${ROOTDIR}/gmp
+QMPDIR=${ROOTDIR}/qmp
+QMTDIR=${ROOTDIR}/qmt
+
+. ${ROOTDIR}/functions.sh
+PREFIX=`make_prefix ${CHROMADIR}/PREFIX ${CHROMADIR}/VERSION ${ARCH}-qmt-pat`
+clean_dir ${PREFIX}
+
+QDP_PREFIX=`make_prefix ${QDPDIR}/PREFIX ${QDPDIR}/VERSION ${QDPARCH}-qmt-pat`
+GMP_PREFIX=`make_prefix ${GMPDIR}/PREFIX ${GMPDIR}/VERSION ${GMPARCH}` 
+QMP_PREFIX=`make_prefix ${QMPDIR}/PREFIX ${QMPDIR}/VERSION xt-pat`
+QMT_PREFIX=`make_prefix ${QMTDIR}/PREFIX ${QMTDIR}/VERSION xt-barcelona-pat`
+echo $QDP_PREFIX
+echo $GMP_PREFIX
+
+
+${CHROMADIR}/chroma/configure --prefix=${PREFIX} \
+			     --host=x86_64-unknown-linux-gnu \
+			     --build=x86_64-suse-linux \
+			     --with-qdp=${QDP_PREFIX} \
+			     CFLAGS="-g -O3 -msse -msse2 -march=barcelona" CXXFLAGS="" LIBS="" \
+			     CC="cc" CXX="CC" \
+	 --enable-sse-wilson-dslash \
+	 --with-gmp=${GMP_PREFIX} \
+         --with-qmp=${QMP_PREFIX} \
+	 --with-qmt=${QMT_PREFIX}
