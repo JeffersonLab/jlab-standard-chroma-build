ba10afa8570bed15f6100a37b2817ed0cfc719d3
edwards <edwards>
2009-04-17 20:42:58 +0000

    Switched to HEAD version.

--- ranger_build.sh
+++ ranger_build.sh
@@ -1,11 +1,34 @@
 #! /bin/bash
 
-#### Setup configure here
-./configure --enable-parallel-make=4 \
-            --enable-install-root=$HOME/install
+install_root="$HOME/install/new"
+mailto_list="edwards@jlab.org"
+parallel_make=8
+build_name="Ranger Build"
+
+echo ""
+echo -n "Starting date: "
+date
+
+./configure  \
+  --enable-qmp-mvia-version=HEAD \
+  --enable-qmp-version=HEAD \
+  --enable-qmt-version=HEAD \
+  --enable-qdp-version=HEAD \
+  --enable-chroma-version=HEAD \
+  --enable-bagel-qdp-version=HEAD \
+  --enable-adat-version=HEAD \
+  --enable-install-root="${install_root}" \
+  --enable-parallel-make="${parallel_make}" \
+  --enable-mailto="${mailto_list}" \
+  --enable-build-name="${build_name}"
 
 ./build.sh libxml2/ranger/ranger \
 	   gmp/ranger/ranger \
 	   qmp/ranger/ranger \
 	   qdp++/ranger/parscalar-ib \
 	   chroma/ranger/parscalar-ib
+
+echo ""
+echo -n "Ending date: "
+date
+
