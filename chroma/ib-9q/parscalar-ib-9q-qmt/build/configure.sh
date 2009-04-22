93f4dbe8fd54401eea10a67226a7a3f7a5a522eb
bjoo <bjoo>
2009-04-22 18:57:27 +0000

    9Q-QMT build

---
+++ chroma/ib-9q/parscalar-ib-9q-qmt/build/configure.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+ARCH=parscalar-ib-9q-qmt
+CHROMADIR=../../..
+ROOTDIR=../../../..
+QDPDIR=${ROOTDIR}/qdp++
+QMPDIR=${ROOTDIR}/qmp
+QMTDIR=${ROOTDIR}/qmt
+. ${ROOTDIR}/functions.sh
+PREFIX=`make_prefix ${CHROMADIR}/PREFIX ${CHROMADIR}/VERSION ${ARCH}`
+clean_dir ${PREFIX}
+
+QDP_PREFIX=`make_prefix ${QDPDIR}/PREFIX ${QDPDIR}/VERSION ${ARCH}`
+QMP_PREFIX=`make_prefix ${QMPDIR}/PREFIX ${QMPDIR}/VERSION ib-9q`
+QMT_PREFIX=`make_prefix ${QMTDIR}/PREFIX ${QMTDIR}/VERSION intel`
+CC="mpicc -cc=gcc"
+CXX="mpiCC -cxx=g++"
+
+${CHROMADIR}/chroma/configure --prefix=${PREFIX} --with-qdp=${QDP_PREFIX} \
+   CFLAGS="-O3 -march=core2 -funroll-loops -fpeel-loops" CXXFLAGS="" \
+   --enable-sse-wilson-dslash  --with-gmp=/usr --enable-testcase-runner=6n_mpirun_rsh  \
+   --with-qmt=${QMT_PREFIX} \
+   --with-qmp=${QMP_PREFIX} CC="${CC}"  CXX="${CXX}" \
+   --enable-opt-eigcg --enable-lapack=lapack LIBS="-llapack -lblas -lgfortran -lgmp" 
+
+#   --enable-lapack=lapack LIBS="-llapack -lblas -lgfortran -lgmp"
