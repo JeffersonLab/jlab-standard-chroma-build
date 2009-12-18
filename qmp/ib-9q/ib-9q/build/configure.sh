c8b74d2d16985929c4b6505ad0e0c9c1c66792a8
Robert Edwards <edwards@qcd9qi01.jlab.org>
2009-12-18 16:22:43 -0500

    For qcd9qi01

--- qmp/ib-9q/ib-9q/build/configure.sh
+++ qmp/ib-9q/ib-9q/build/configure.sh
@@ -5,5 +5,7 @@ PREFIX=`make_prefix ../../../PREFIX ../../../VERSION ib-9q`
 clean_dir ${PREFIX}
 
 
-CC="mpicc -cc=gcc" 
-../../../qmp/configure --prefix=${PREFIX} --with-qmp-comms-type=MPI --with-qmp-comms-cflags="" --with-qmp-comms-ldflags="" --with-qmp-comms-libs="" CC="${CC}" CFLAGS=" -O3 -march=core2" 
+CC="/usr/mpi/gcc/mvapich-1.1.0/bin/mpicc -cc=gcc" 
+CXX="/usr/mpi/gcc/mvapich-1.1.0/bin/mpiCC" 
+
+../../../qmp/configure --prefix=${PREFIX} --with-qmp-comms-type=MPI --with-qmp-comms-cflags="" --with-qmp-comms-ldflags="" --with-qmp-comms-libs="" CC="${CC}" CFLAGS=" -O3 -march=core2" LDFLAGS="-L/usr/mpi/gcc/mvapich-1.1.0/lib" LDFLAGS="-static"
