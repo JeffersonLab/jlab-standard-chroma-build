52a43d0d86c5af2db5a9bb87f28a4b5ff7df0ea1
Robert Edwards <edwards@qcd9qi01.jlab.org>
2009-12-27 16:29:33 -0500

    Turned off "-g".

--- chroma/scalar/scalar-9q-qmt/build/configure.sh
+++ chroma/scalar/scalar-9q-qmt/build/configure.sh
@@ -12,7 +12,7 @@ clean_dir ${PREFIX}
 QDP_PREFIX=`make_prefix ${QDPDIR}/PREFIX ${QDPDIR}/VERSION scalar-9q-qmt`
 QMT_PREFIX=`make_prefix ${QMTDIR}/PREFIX ${QMTDIR}/VERSION intel`
 ${CHROMADIR}/chroma/configure --prefix=${PREFIX} --with-qdp=${QDP_PREFIX} \
-	CFLAGS="-g -O3 -fargument-noalias-global -funroll-all-loops -fpeel-loops -march=core2" \
+	CFLAGS="-O3 -fargument-noalias-global -funroll-all-loops -fpeel-loops -march=core2" \
 	CXXFLAGS="" \
         --enable-sse-wilson-dslash --with-gmp=/usr CC=gcc CXX=g++  \
 	--with-qmt=$QMT_PREFIX
--- qdp++/scalar/scalar-9q-qmt/build/configure.sh
+++ qdp++/scalar/scalar-9q-qmt/build/configure.sh
@@ -11,6 +11,6 @@ QMT_PREFIX=`make_prefix ${QMTDIR}/PREFIX ${QMTDIR}/VERSION intel`
 clean_dir ${PREFIX}
 
 ${QDPDIR}/qdp++/configure --prefix=${PREFIX} --enable-parallel-arch=scalar --enable-sse2 --with-qmt=${QMT_PREFIX} \
-	CXXFLAGS="-g -fargument-noalias-global -O3 -finline-limit=50000 -march=core2 -funroll-all-loops -fpeel-loops" \
-	CFLAGS="-g -fargument-noalias-global -O3 -march=core2 -funroll-all-loops -fpeel-loops" \
+	CXXFLAGS="-fargument-noalias-global -O3 -finline-limit=50000 -march=core2 -funroll-all-loops -fpeel-loops" \
+	CFLAGS="-fargument-noalias-global -O3 -march=core2 -funroll-all-loops -fpeel-loops" \
 	CXX=g++ CC=gcc
