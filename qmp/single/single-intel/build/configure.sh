9621dcf7174c70c3c0b9b1e0d90279f5f8f3e529
Balint Joo <bjoo@jlab.org>
2013-08-27 22:09:01 -0400

    Added build configurations for parscalar-single and parscalar-single-double using the Intel compiler (-O2) optimization.  Added this into nightly builds.

--- qmp/single/single-intel/build/configure.sh
+++ qmp/single/single-intel/build/configure.sh
@@ -7,4 +7,4 @@ clean_dir ${PREFIX}
 source /opt/intel/composerxe/bin/compilervars.sh intel64
 CC=icc
 
-../../../qmp/configure --prefix=${PREFIX} --with-qmp-comms-type=SINGLE CC=${CC} CFLAGS="-O2 -xhost -std=gnu99"
+../../../qmp/configure --prefix=${PREFIX} --with-qmp-comms-type=SINGLE CC=${CC} CFLAGS="-openmp -O2 -xhost -std=gnu99"
