3f48b3e9b80c261d2a5ca8c06f88e037828473f4
Robert Edwards <edwards@jlab.org>
2013-11-09 14:20:26 -0500

    Support c++-11.

--- scalar.sh
+++ scalar.sh
@@ -1,22 +1,38 @@
-#!/bin/bash -x
 
-install_root="/home/hwlin/install"
-parallel_make=4
-build_name="Test  Build"
+#!/bin/bash -x
+install_root="/dist/scidac"
+mailto_list="edwards@jlab.org"
+parallel_make=16
+build_name="9q Nightly Build"
 
 echo ""
 echo -n "Starting date: "
 date
 
 ./configure  \
-  --enable-chroma-version=HEAD \
+  --enable-qmp-version=master \
+  --enable-qmt-version=master \
+  --enable-qdp-version=master \
+  --enable-chroma-version=master \
+  --enable-bagel-qdp-version=master \
+  --enable-adat-version=master \
+  --enable-colorvec-version=master \
+  --enable-sfpcac-version=master \
+  --enable-redstar-version=master \
   --enable-install-root=${install_root} \
   --enable-parallel-make="${parallel_make}" \
-  --enable-build-name="${build_name}"
+  --enable-build-name="${build_name}" \
+  --enable-mailto="${mailto_list}" 
 
+/bin/rm -rf \
+    ${install_root}/qdp++/master \
+    ${install_root}/chroma/master
 
-./build.sh qdp++/scalar/scalar-7n \
-            chroma/scalar/scalar-7n
+./build-git.sh  \
+    qdp++/scalar/scalar-9q \
+    qdp++/ib-9q/parscalar-ib-9q \
+    chroma/scalar/scalar-9q \
+    chroma/ib-9q/parscalar-ib-9q 
 
 echo ""
 echo -n "Ending date: "
