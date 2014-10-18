cda2aef9f1abc7e0f7c49079a403a28498017f29
Robert Edwards <edwards@jlab.org>
2014-10-18 12:42:17 -0400

    Fixed some more typos.

--- redstar/scalar/scalar-9q/build/configure.sh
+++ redstar/scalar/scalar-9q/build/configure.sh
@@ -4,16 +4,19 @@ ARCH=scalar-9q
 REDSTARDIR=../../..
 ROOTDIR=../../../..
 ADATDIR=${ROOTDIR}/adat
+HADRONDIR=${ROOTDIR}/hadron
 
 . ${ROOTDIR}/functions.sh
 PREFIX=`make_prefix ${REDSTARDIR}/PREFIX ${REDSTARDIR}/VERSION ${ARCH}`
 clean_dir ${PREFIX}
 
 ADAT_PREFIX=`make_prefix ${ADATDIR}/PREFIX ${ADATDIR}/VERSION ${ARCH}`
+HADRON_PREFIX=`make_prefix ${HADRONDIR}/PREFIX ${HADRONDIR}/VERSION ${ARCH}`
 
 MYCC=gcc
 MYCXX=g++
 
 ${REDSTARDIR}/redstar/configure --prefix=${PREFIX} \
+   --with-hadron=${HADRON_PREFIX} \
    --with-adat=${ADAT_PREFIX}
 
