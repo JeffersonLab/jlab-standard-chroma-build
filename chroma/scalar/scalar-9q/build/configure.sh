fdd696faff85df237473327b2305efc5a5576f2e
Robert Edwards <edwards@jlab.org>
2016-02-01 09:57:03 -0500

    Added GMP to chroma scalar build.

--- chroma/scalar/scalar-9q/build/configure.sh
+++ chroma/scalar/scalar-9q/build/configure.sh
@@ -22,9 +22,10 @@ QDP_PREFIX=`make_prefix ${QDPDIR}/PREFIX ${QDPDIR}/VERSION ${ARCH}`
 ${CHROMADIR}/chroma/configure --prefix=${PREFIX} --with-qdp=${QDP_PREFIX} \
 	CFLAGS="-O3 -fargument-noalias-global -funroll-all-loops -fpeel-loops -march=core2" \
 	CXXFLAGS="" \
-        --enable-sse-wilson-dslash CC=gcc CXX=g++ \
-        --enable-lapack=lapack --enable-opt-eigcg LIBS="-llapack -lblas -lgfortran"
+        --enable-sse-wilson-dslash --with-gmp=/usr CC=gcc CXX=g++
+        --enable-lapack=lapack --enable-opt-eigcg LIBS="-llapack -lblas -lgfortran -lgmp"
 
-#        --enable-sse-wilson-dslash --with-gmp=/usr CC=gcc CXX=g++
-#        --enable-lapack=lapack --enable-opt-eigcg LIBS="-llapack -lblas -lgfortran -lgmp"
+#        --enable-sse-wilson-dslash CC=gcc CXX=g++ 
+#        --enable-lapack=lapack --enable-opt-eigcg LIBS="-llapack -lblas -lgfortran"
+
 
