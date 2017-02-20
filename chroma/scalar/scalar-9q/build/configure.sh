6bc6a9733f09d8c8b23ab0301ab5bb25b91942ea
Robert Edwards <edwards@jlab.org>
2017-02-20 11:43:21 -0500

    Changed configure script for chroma/scalar-9q

--- chroma/scalar/scalar-9q/build/configure.sh
+++ chroma/scalar/scalar-9q/build/configure.sh
@@ -12,6 +12,9 @@ popd
 export PATH=/dist/gcc-4.8.2/bin:$PATH
 export LD_LIBRARY_PATH=/dist/gcc-4.8.2/lib64:/dist/gcc-4.8.2/lib:$LD_LIBRARY_PATH
 
+CXX=/dist/gcc-4.8.2/bin/g++
+CC=/dist/gcc-4.8.2/bin/gcc
+
 . ${ROOTDIR}/functions.sh
 PREFIX=`make_prefix ${CHROMADIR}/PREFIX ${CHROMADIR}/VERSION ${ARCH}`
 clean_dir ${PREFIX}
@@ -23,7 +26,7 @@ GMP=/dist/gmp-6.0.0
 ${CHROMADIR}/chroma/configure --prefix=${PREFIX} --with-qdp=${QDP_PREFIX} \
 	CFLAGS="-O3 -fargument-noalias-global -funroll-all-loops -fpeel-loops -march=core2" \
 	CXXFLAGS="" \
-        --enable-sse-wilson-dslash --with-gmp=$GMP CC=gcc CXX=g++ \
+        --enable-sse-wilson-dslash --with-gmp=$GMP CC=$CC CXX=$CXX \
         --enable-lapack=lapack --enable-opt-eigcg LIBS="-llapack -lblas -lgfortran ${GMP}/lib/libgmp.a"
 
 #        --enable-sse-wilson-dslash CC=gcc CXX=g++ 
